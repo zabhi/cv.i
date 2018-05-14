f84a79139c9e36e614c2c833fa423cb19c2c2889
Abhishek Kumar <labs.abhi@gmail.com>
2018-05-14 19:25:58 +0530

    :)

--- src/cv.py
+++ src/cv.py
@@ -14,19 +14,11 @@ opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
 sure_bg = cv2.dilate(opening,kernel, iterations=3)
 # Finding sure foreground area
 dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7 * dist_transform.max(), 255, 0)
+ret, sure_fg = cv2.threshold(dist_transform, 0.25 * dist_transform.max(), 250, 0)
 
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
+print('Number of white pixels:', np.sum(sure_fg > 220))
+print('Number of black pixels:', np.sum(sure_fg < 220))
 
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-print(ret)
-cv2.imshow('image', ret)
+cv2.imshow('image', sure_fg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
